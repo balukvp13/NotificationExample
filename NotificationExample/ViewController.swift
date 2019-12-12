061dcf55bd2817f128fc0b2fdf2f9d676ed48163
Bala Krishna <balakrishna@balas-macbook-pro.local>
2019-12-12 11:46:02 +0530

    Initial Commit

---
+++ NotificationExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NotificationExample
+//
+//  Created by Bala Krishna on 12/12/19.
+//  Copyright © 2019 Bala Krishna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
