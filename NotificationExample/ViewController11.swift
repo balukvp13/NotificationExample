e4cd4e38cce0a45fd76670603224e37fe6dcae49
Bala Krishna <balakrishna@balas-macbook-pro.local>
2020-02-05 12:17:59 +0530

    add one more to view to tabor

---
+++ NotificationExample/ViewController11.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController11.swift
+//  NotificationExample
+//
+//  Created by Bala Krishna on 05/02/20.
+//  Copyright © 2020 Bala Krishna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController11: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.title = "New Item"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
