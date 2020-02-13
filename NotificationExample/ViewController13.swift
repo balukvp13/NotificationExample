8cc4183f4cf1f11c269bc0cf1802eb7be50b2c62
Bala Krishna <balakrishna@balas-macbook-pro.local>
2020-02-13 12:17:55 +0530

    add viewcontrollerin project

---
+++ NotificationExample/ViewController13.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController13.swift
+//  NotificationExample
+//
+//  Created by Bala Krishna on 13/02/20.
+//  Copyright © 2020 Bala Krishna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController13: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
